5432c5578d31370f62859d3fb72c325f5552fd5b
Mackenzie Kary <mackenziekary@mackenzkarysmbp.hitronhub.home>
2017-10-23 20:30:04 -0600

    Logic added for: getClassName, getSuperclassName, getInterfaceNames, getClassMethods

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 
 public class Inspector {
 
@@ -7,27 +8,69 @@ public class Inspector {
 	}
 	
 	// Get the name of the class
-	public String getClassName(){
-		return null;
+	public String getClassName(Class classInput){
+		Class reflectionClass = classInput.getClass();
+		String className = reflectionClass.getName();
+		return className;
 	}
 	
 	// Get the name of the superclass
-	public String getSuperclassName(){
-		return null;
+	public String getSuperclassName(Class superclassInput){
+		Class reflectionSuperClass = superclassInput.getSuperclass();
+		String superClassName = reflectionSuperClass.getName();
+		return superClassName;
 	}
 	
 	// Get the name of the interfaces
-	public String getInterfaceNames(){
+	public String getInterfaceNames(Class classInput){
+		Class reflectionClass = classInput.getClass();
+		Class[] interfaces = reflectionClass.getInterfaces();
+		
+		for(Class classInterface : interfaces){
+			System.out.println("Interface Name: " + classInterface.getName());
+		}
 		return null;
 	}
 	
 	// Get the methods in class and their info
-	public String getClassMethods(){
+	public String getClassMethods(Class classInput){
 		// Return the following:
+			// Name
 			// Exceptions thrown
 			// Parameter Type
 			// Return Type
 			// Modifiers
+		Class reflectionClass = classInput.getClass();
+		Method[] classMethods = reflectionClass.getMethods();
+		
+		for (Method classMethod : classMethods){
+			
+			// Get method name 
+			String methodName = classMethod.getName();
+			System.out.println("Method name: " + methodName);
+			
+			// Get Method Exception Types 
+			Class[] exceptions = classMethod.getExceptionTypes();
+			for (Class exception : exceptions){
+				String exceptionType = exception.getName();
+				System.out.println("\tException type: " + exceptionType);
+			}
+			
+			// Get Parameter Types
+			Class[] parameters = classMethod.getParameterTypes();
+			for (Class parameter : parameters){
+				String parameterType = parameter.getName();
+				System.out.println("\tParameter type: " + parameterType);
+			}
+			
+			// Get Return Type
+			Class returnType = classMethod.getReturnType();
+			System.out.println("\tReturn Type: " + returnType);
+			
+			// Get Modifiers
+			int methodModifiers = classMethod.getModifiers();
+			System.out.println("\tModifiers: " + methodModifiers);
+		}
 		return null;
 	}
 	
