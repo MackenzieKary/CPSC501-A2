482abfb1d7bbd432ba7d418ef40b60132492cc09
Mackenzie Kary <mackenziekary@mackenzkarysmbp.hitronhub.home>
2017-10-23 18:12:53 -0600

    Added necessary files. Created Inspector class and initial information getter methods

---
+++ src/Inspector.java
@@ -0,0 +1,56 @@
+
+public class Inspector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// Get the name of the class
+	public String getClassName(){
+		return null;
+	}
+	
+	// Get the name of the superclass
+	public String getSuperclassName(){
+		return null;
+	}
+	
+	// Get the name of the interfaces
+	public String getInterfaceNames(){
+		return null;
+	}
+	
+	// Get the methods in class and their info
+	public String getClassMethods(){
+		// Return the following:
+			// Exceptions thrown
+			// Parameter Type
+			// Return Type
+			// Modifiers
+		return null;
+	}
+	
+	// Get the constructors in class and their info
+	public String getClassConstructors(){
+		// Return the following:
+			// Parameter Types
+			// Modifiers
+		return null;
+	}
+	
+	// Get the fields in the class and their info
+	public String getClassFields(){
+		// Return the following:
+			// Type
+			// Modifiers
+		return null; 
+	}
+	
+	// Get the values of the fields within the class
+	public String getClassFieldsValues(){
+		// Return current value of each field
+		return null;
+	}
+	
+}
